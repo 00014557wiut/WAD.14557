12caa4cd80bb332b294b38e69b474496c6d02df1
00014557wiut <takhirjanovd@yahoo.com>
2024-03-09 00:42:41 +0500

    Models created

---
+++ WAD.CW1.14557/Models/Issue.cs
@@ -0,0 +1,13 @@
+﻿namespace WAD.CW1._14557.Models
+{
+	public class Issue
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Status { get; set; }
+		public DateTime Created { get; set; }
+		public int TrakerId { get; set; }
+		public Tracker Tracker { get; set; }
+	}
+}
---
+++ WAD.CW1.14557/Models/Tracker.cs
@@ -0,0 +1,10 @@
+﻿namespace WAD.CW1._14557.Models
+{
+	public class Tracker
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime UpdateDate { get; set; }
+		public ICollection<Issue> Issues { get; set; }
+	}
+}
