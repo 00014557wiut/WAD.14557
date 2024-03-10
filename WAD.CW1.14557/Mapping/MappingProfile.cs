c1c46c2a292a3d2cd4a30bb07e4ae422c2cd88cb
00014557wiut <takhirjanovd@yahoo.com>
2024-03-10 23:47:14 +0500

    Mapping profiles

---
+++ WAD.CW1.14557/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using WAD.CW1._14557.Models;
+using WAD.CW1._14557.Dtos;
+
+namespace WAD.CW1._14557.Mapping
+{
+	public class MapperProfile : Profile
+	{
+		public MapperProfile()
+		{
+			CreateMap<Issue, IssueDto>();
+			CreateMap<IssueDto, Issue>();
+			CreateMap<Comment, CommentDto>();
+			CreateMap<CommentDto, Comment>();
+		}
+	}
+}
