e2b5eb503bddc0cfe10543c24c65120441a79ade
00014557wiut <takhirjanovd@yahoo.com>
2024-03-12 13:45:15 +0500

    Added interfaces

---
+++ DAL/Interfaces/ICommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD.CW1._14557.Models;
+
+namespace WAD.CW1._14557.DAL.Interfaces
+{
+    public interface ICommentRepository
+    {
+		Task<IEnumerable<Comment>> GetCommentsByIssueIdAsync(int issueId);
+		Task<Comment> GetCommentByIdAsync(int id);
+		Task<Comment> CreateCommentAsync(Comment comment);
+		Task UpdateCommentAsync(Comment comment);
+		Task DeleteCommentAsync(int id);
+	}
+}
