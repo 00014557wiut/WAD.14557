1b26560799908b08624409aa1a3f70c928ca9310
00014557wiut <takhirjanovd@yahoo.com>
2024-03-12 13:37:45 +0500

    Dal layer config with application db context

--- DAL/AppData/ApplicationDbContext.cs
+++ DAL/AppData/ApplicationDbContext.cs
@@ -1,6 +1,28 @@
-﻿namespace WAD.CW1._14557.AppData
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.CW1._14557.Models;
+
+namespace WAD.CW1._14557.AppData
 {
-	public class ApplicationDbContext
+	public class ApplicationDbContext : DbContext
 	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+		{
+		}
+
+		public DbSet<Issue> Issues { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// If you have any fluent API configurations, they would go here.
+			// For example, to configure a one-to-many relationship between Issue and Comment:
+			modelBuilder.Entity<Issue>()
+				.HasMany(i => i.Comments)
+				.WithOne(c => c.Issue)
+				.HasForeignKey(c => c.IssueId);
+		}
 	}
 }
---
+++ DAL/ConfigurationServiceDAL.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD.CW1._14557.AppData;
+
+namespace WAD.CW1._14557.DAL
+{
+    public static class ConfigurationServiceDAL
+    {
+		public static IServiceCollection ConfigurationDAL(
+			this IServiceCollection services,
+			IConfiguration configuration
+		)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+			return services;
+		}
+		
+    }
+}
